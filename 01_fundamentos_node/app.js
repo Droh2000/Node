f41d12e28c7964a140ec550ed754b22b26b33fb9
gomezfarm <gomez.farm@outlook.com>
2025-04-27 18:25:38 -0600

    Seccion 2: Fundamentos de Node

---
+++ 01_fundamentos_node/app.js
@@ -0,0 +1,8 @@
+
+const message = 'Hola Mundo';
+
+console.log(message);
+
+// Para ejecutar este archivo no requerimos abrirlo en el navegador con un HTML
+// Solo desde la terminal dentro de esta carpeta ponemos:
+//      * node app (app es el nombre del archivo) 
