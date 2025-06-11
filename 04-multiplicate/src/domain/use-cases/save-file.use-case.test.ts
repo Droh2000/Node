27cc80c1702f3de669d5c509dbfb22d23b05a099
gomezfarm <gomez.farm@outlook.com>
2025-06-10 19:56:43 -0600

    SpyOn + Mock Implementation - Para cambiar la logica de funciones y limpiar despues de su uso

--- 04-multiplicate/src/domain/use-cases/save-file.use-case.test.ts
+++ 04-multiplicate/src/domain/use-cases/save-file.use-case.test.ts
@@ -12,6 +12,17 @@ describe('SaveFileUseCase', () => {
     }
     // Esta es la ruta que queremos evaluar, con la extencion del archivo
         const filePath = `${options.fileDestination}/${options.fileName}.txt`;
+    
+    // IMPORTANTE: Al usar el "spyOn" el "mockImplementation" persiste sobre la siguiente prueba (Todas las pruebas se hacen secuencialmente, asi que despues 
+    // de esta prueba las demas fallaran) tenemos que limpiar las funciones
+    // Esta funcion se ejecuta antes de cada prueba
+    /*beforeEach(() => {
+        // Esta logica solo funciona para esta implementacion
+        const logMock = jest.fn();
+        // Pero no para nuestra logica actual
+        jest.clearAllMocks();
+    });*/
+    // Cuando tenemos un mockImplementation manualmente tenemos que hacer la limpieza nosotros
 
     // Solucion del Falso positivo en el que analizar la carpeta "outputs/Archivo" ya creado de un test anterior
     // para esto en las pruebas hay ciclos de vida para que pase algo, antes,durante, despues de las pruebas
@@ -62,6 +73,43 @@ describe('SaveFileUseCase', () => {
         expect( result ).toBe( true );
         expect( fileExists ).toBe( true );
         expect( fileContent ).toBe( options.fileContent );
-    });    
+    });
+    
+    // Podemos probar el escenario donde no se cree el archivo (Evaluamos los posibles fallos, puede que sea una de las dos lineas
+    // mkdirSync o writeFileSync) en esos caso entraria al Catch
+    // Podemos simular que esto falle (Empezamos con el directorio porque de eso se trata la primera linea de codigo)
+    test('should return false if directory could not be created', () => {
 
+        const saveFile = new SaveFile();
+        // Podemos crearnos un SPY sobre el "mkdirSync" espiando el FS, solo con el "spyOn" espiamos que la funcion haya sido llamda, los argumentos, pero con el 
+        // "mockImplementation" significa que queremos sobrescribir la funcionalidad de la funcion por la logica que le especificamos dentro de los parentesis
+        const mkdirSpy = jest.spyOn(fs, 'mkdirSync').mockImplementation(
+            () => { throw new Error('This is a custom error message from testing') } // Cuando se llama la funcion esto es lo que se ejecutara
+        );
+
+        // Da igual las opciones con las que le mandemos
+        const result = saveFile.execute(options);
+
+        // Esperamos que el resultado falle (En la logica al fallar estamos esperando que nos regresa false)
+        expect( result ).toBe( false );
+
+        // Cuando tenemos un "mockImplementation" manualmente tenemos que hacer la limpieza aqui, no dentro de un beforeEach
+        // esto nos restaura la funcion sobrescrita a su logica original
+        mkdirSpy.mockRestore();
+    });
+
+    // Evaluamos la segunda linea de codigo "writeFileSync"
+    test('should return false if file could not be created', () => {
+
+        const saveFile = new SaveFile();
+        
+        const writeFileSpy = jest.spyOn(fs, 'writeFileSync').mockImplementation(
+            () => { throw new Error('This is a custom writing error message from testing') } // Cuando se llama la funcion esto es lo que se ejecutara
+        );
+
+        const result = saveFile.execute({ fileContent: 'Hola' });
+
+        expect( result ).toBe( false );
+        writeFileSpy.mockRestore();
+    });
 });
