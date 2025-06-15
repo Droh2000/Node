09fa441ecb4fc0b0d86a39107eaf2c95fcdb37dd
gomezfarm <gomez.farm@outlook.com>
2025-06-15 11:42:30 -0600

    CRON Task - Paquete para monitorear la aplicacion cada cierto tiempo

--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -1,8 +1,19 @@
+import { CronJob } from 'cron';
 
 export class Server {
 
     // Este es el punto de inicio de nuestra aplicacion
     public static start(){
         console.log('Server started...');
+
+        // Esto lo pusimos aqui como muestra de ejemplo
+        const job = new CronJob(
+            '*/3 * * * * *', // cronTime (Aqui cambiamos segun por cada cuanto tiempo queremos ejecutarlo)
+            () => {
+                const date = new Date();
+                console.log('3 second', date);
+            },
+        );
+        job.start();
     }
 }
