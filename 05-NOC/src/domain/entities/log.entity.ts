b7aba3ff5ba8b667e6172494b3f09cb873fbb611
gomezfarm <gomez.farm@outlook.com>
2025-06-16 17:15:29 -0600

    LogEntity - Informacion que fluye de nuestros Logs

---
+++ 05-NOC/src/domain/entities/log.entity.ts
@@ -0,0 +1,28 @@
+// Las entidades las vamos a ver como algo que va a llegar a las base de datos
+// tecnicamente los que guardamos en la base de datos
+
+// Podriamos crearnos un tipo pero para variar nos creamos una enumeracion
+// lo exportamos para despues evaluar basado en los datos que recibamos
+export enum LogServerityLevel {
+    low = 'low',
+    medium = 'medium',
+    high = 'high',
+}
+
+export class LogEntity {
+    // Aqui tenemos lo que queremos registrar en la aplicacion
+    // esto es lo que estariamos almacando en el sistema de logs
+    public level: LogServerityLevel; // Este es nuestro nivel de errores de entidades
+    public message: string;
+    public createdAt: Date;
+
+    // Hay diferentes formas de inicializar las propiedades
+    constructor( message: string, level: LogServerityLevel ){
+        this.message = message;
+        this.level = level;
+        this.createdAt = new Date();
+    }
+
+    // Asi si en el futuro cambiamos nuestro origen de datos, no nos va a afectar porque ese origen va a mapear a esta entidad
+    // y esto es lo que vamos a usar, no vamos a usar un objeto de la base de datos directamente
+}
