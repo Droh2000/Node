dbedadc97cdba93b6c66574de6d31e575e7b51bd
gomezfarm <gomez.farm@outlook.com>
2025-07-17 10:56:49 -0600

    Crear y Leer desde Mongo

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -1,5 +1,5 @@
 import { envs } from "./config/plugins/envs.plugin";
-import { MongoDatabase } from "./data/mongo";
+import { LogModel, MongoDatabase } from "./data/mongo";
 import { Server } from "./presentation/server";
 
 (async ()=>{
@@ -12,6 +12,21 @@ async function main(){
         mongoUrl: envs.MONGO_URL,
         dbName: envs.MONGO_DB_NAME,
     });
+
+    // No van a permanecer aqui las instrucciones pero vamos a probarlas
+    // Crear una coleccion que seria igual a las tablas en una BD relacional y un Documento seria un Registro
+    /*const newLog = await LogModel.create({
+        // Aqui definimos lo que requerimos para crear un registro que vaya a almacenarse a la coleccion de logs
+        message: 'Test Message desde Mongo',
+        origin: 'App.ts',
+        level: 'low',
+    });
+
+    await newLog.save();*/
+
+    // Para obtener los registros (Por defecto nos los da en formato de arreglo)
+    // const logs = await LogModel.find();
+    // console.log(logs);
 
     Server.start();
     // Ejemplo de como acceder a las variables (Gracias a la libreria tenemos el tipado las validaciones pero tambien si no especificamos
