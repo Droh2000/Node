21cb3742ac0ea64020795b21366e2e3fc3678218
gomezfarm <gomez.farm@outlook.com>
2025-07-20 12:45:06 -0600

    Arreglar el router de una app de React configurando en Node el direccionamiento

--- 07-rest-web/src/presentation/server.ts
+++ 07-rest-web/src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'; // Para usarlo tenemos que instalar el archivo de definicion de TS
+import path from 'path';
 
 export class Server {
 
@@ -11,6 +12,22 @@ export class Server {
         // Usaremos un middleware que son funciones que se ejecutan en todo momento que se pasa por una ruta, ademas colocaremos
         // en su disposicion a los usuarios que lo soliciten, la carpeta public
         this.app.use( express.static( 'public' ) );
+
+        // En esta aplicacion de WebServer vamos a usar una aplicacion de React que se creo pero solo son los archivos
+        // que se subirian a produccion, el problema que tenemos es su sistema de rutas nuevo, que mientras nos movemos en la aplicacion todo funciona
+        // correctamente pero si en cualquier ruta actualizamos la aplicacion, se pierde y nos dice que recurso no encontrado, esto es lo que queremos
+        // arreglar en la aplicacion del WebServer (Estos nuevos archivos los metimos a la carpeta Public)
+        // Este problema pasa porque no hay ningun directorio en la carpeta publica con el nombre de la ruta "/ruta" (Aqui es donde express nos ayuda)
+        // Indicamos que cualquier otra peticion GET que no sea de la carpeta public (Esto es para interceptar todas las request y emitir una response)
+        this.app.get('*', (req, res) => {
+            // La aplicacion primero entra a "Public", si lo encuentra ahi lo sirve y ahi React toma el control de la aplicacion y hace todo del lado del cliente
+            // El problema es cuando se recarga el navegador en una ruta que no es el Root de la aplicacion, entonces cae dentro de este metodo
+            // Vamos a retornar el Path al index que tenemos en la carpeta public pero tiene que ser un path absoluto
+            // Es "__dirname" porque tiene que ser un path absoluto y son 3 "../../../" porque estamos en server.ts y queremos llegar al root del proyecto
+            const indexPath = path.join( __dirname + '../../../public/index.html' );
+            res.sendFile(indexPath); // solo podemos llamar esto una vez
+            // Con esto podemos recargar en cualquier parte la aplicacion y no falla porque el Router de React tiene el control
+        });
 
         // Ponemos la aplicacion a escuchar peticiones
         this.app.listen(3000, () => {
