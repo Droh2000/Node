e7272192a4b7143985380b1e969dc8dc66d93a46
gomezfarm <gomez.farm@outlook.com>
2025-08-06 18:17:46 -0600

    Crear category en base de dato - implementado el servicio y usarlo en el controlador

--- 10-autorizacion-validacion-autenticacion/src/presentation/category/controller.ts
+++ 10-autorizacion-validacion-autenticacion/src/presentation/category/controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express';
 import { CustomError } from '../../domain';
 import { CreateCategoryDto } from '../../domain/dtos/category/create-category.dto';
+import { CategoryService } from '../services/category.service';
 
 export class CategoryController {
 
-    constructor(){}
+    constructor(
+        // Le pasamos el servicio al controlador
+        private readonly categoryService: CategoryService,
+    ){}
 
     // Manejar los errores
     private handleError = (error: unknown, res: Response) => {
@@ -18,13 +22,19 @@ export class CategoryController {
     }
 
     // Metodos que se llaman en el archivo de rutas
-    createCategory = async (req: Request, res: Response) => {
+    createCategory = (req: Request, res: Response) => {
         // Usamos el Dto para transformar la data que nos mandan en el Body
         const [error, createCategoryDto] = CreateCategoryDto.create( req.body );
         if( error ) return res.status(400).json({ error });
-        
+
         // Si nos preguntamos como obtenemos el usuario, este lo pusimos en "req.body.user" entonces de ahi lo leyemos
         // res.json( req.body );
+
+        // llamamos el servicio
+        this.categoryService.createCategory(createCategoryDto!, req.body.user)
+            // Como buenas practicas de Express es no usar Asyn/Await dentro de los controladores
+            .then( category => res.status(201).json( category ))
+            .catch( error => this.handleError(error, res) );
     }
 
     getCategories = async (req: Request, res: Response) => {
--- 10-autorizacion-validacion-autenticacion/src/presentation/services/category.service.ts
+++ 10-autorizacion-validacion-autenticacion/src/presentation/services/category.service.ts
@@ -1,1 +1,39 @@
+import { CategoryModel } from '../../data';
+import { CustomError, UserEntity } from '../../domain';
+import { CreateCategoryDto } from '../../domain/dtos/category/create-category.dto';
 // La ruta llama al controlador y del controlador llama al servicio
+
+export class CategoryService {
+
+    // Creamos para que se pueda aplicar la inyeccion de dependencias para que asi se pueda
+    // cambiar la Base de datos o el patron repositorio
+    constructor(){}
+
+    // Queremos crear la categoria que esta en "category.model" por tanto ahi vemos que datos son los que nesecitamos aqui
+    // tomando en cuenta que los campos "available" viene del Dto y el User ya lo sacamos
+    // el middleware se encargara de extraer los datos y proveer el mismo tipo de dato
+    async createCategory( createCategoryDto: CreateCategoryDto, user: UserEntity ){
+        // Verificamos si la categoria existe
+        const categoryExists = await CategoryModel.findOne({ name: createCategoryDto.name });
+        if ( categoryExists ) throw CustomError.badRequest( 'Category already exists' );
+
+        try {
+            const category = new CategoryModel({
+                ...createCategoryDto, // Le pasamos todas las propiedades (Porque ya todas estan validadas y tienen el formato correcto)
+                user: user.id, // Solo esta data queremos pasar del usuario
+            });
+
+            await category.save();
+
+            // Para que no nos salga informacion de mas, mejor especificamos aqui los datos que queremos mostrar
+            return {
+                id: category.id,
+                name: category.name,
+                available: category.available,
+            }
+        } catch (error) {
+            // Estos errores hay que cuidarlos porque a cualquier usuario que este consumiendo la API le dara informacion del sistema
+            throw CustomError.internalServer(`${ error }`);
+        }
+    }
+}
