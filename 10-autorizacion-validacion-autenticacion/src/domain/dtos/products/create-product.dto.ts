07204bcd01faf513686bc621dd84cba49a8e1ca0
gomezfarm <gomez.farm@outlook.com>
2025-08-11 11:57:43 -0600

    Crear el Product Dto

---
+++ 10-autorizacion-validacion-autenticacion/src/domain/dtos/products/create-product.dto.ts
@@ -0,0 +1,45 @@
+
+export class CreateProductDto {
+
+    // La misma instancia se creara internamente desde la clase
+    private constructor(
+        // El nombre de las propiedades nos basamos en el modelo que tengamos
+        public readonly name: string,
+        public readonly available: boolean,
+        public readonly price: string,
+        public readonly description: string,
+        // El tipo de datos del usuario esta relacionado con su respectivo DTO porque es la informacion que estamos esperando recibir
+        // no esperamos que nos mande el usuario, solo esperamos tenerlo para poder crear un producto
+        public readonly user: string, // La idea es que sea un String donde se va a contener el ID
+        public readonly category: string, // esta es la misma idea del ID
+    ){}
+
+    static create( props: { [key: string]: any } ): [string?, CreateProductDto?]{
+        const {
+            name,
+            available,
+            price,
+            description,
+            user,
+            category,
+        } = props;
+
+        // Verificacion de como viene la data (Para esto siempre nos guiaremos del modelo y creemos esta capa de proteccion)
+        if( !name ) return ['Missing name'];
+        if( !user ) return ['Missing user'];
+        if( !category ) return ['Missing category'];
+
+        return [
+            undefined,
+            new CreateProductDto(
+                name,
+                // Si viene cualquier valor, por ejemplo un String lo consideramos con string si no es False y con la doble negacion sigue siendo False
+                !!available,// Esta es una forma rapida, igual podemos evaluar como en otros DTOs
+                price,
+                description,
+                user,
+                category,
+            )
+        ];
+    }
+}
